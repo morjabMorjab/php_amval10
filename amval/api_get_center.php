bde176718ccb99d34dfd96b4bed16aa6eac4f5bb
morjab <71502003+morjabMorjab@users.noreply.github.com>
2026-07-23 15:22:41 +0330

    Full Migration from amval10.ir

---
+++ amval/api_get_center.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'config/database.php'; require_once 'includes/functions.php'; checkAuth();
+header('Content-Type: application/json; charset=utf-8');
+$db = getDB(); $stmt = $db->prepare("SELECT * FROM centers WHERE id=?"); $stmt->execute([$_GET['id']]);
+echo json_encode($stmt->fetch(), JSON_UNESCAPED_UNICODE);
+?>
